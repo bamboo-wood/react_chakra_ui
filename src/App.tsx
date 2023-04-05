9198600b8b1db1992a6c19261b4446599ac28cce
bamboo-wood <cantate.1009.0417@gmail.com>
2023-04-05 15:27:28 +0900

    set up routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import { Button, ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+
 import theme from "./theme/theme";
+import { Router } from "./router/Router";
+
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Button colorScheme="teal">Hello World</Button>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React, { FC, memo } from "react";
+import { Outlet } from "react-router";
 
 export const Home: FC = memo(() => {
   return (
