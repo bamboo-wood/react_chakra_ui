afbb026608955540e58bb436e9d5482d671e549e
bamboo-wood <cantate.1009.0417@gmail.com>
2023-04-05 16:11:13 +0900

    add Drawer

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -2,41 +2,66 @@ import { FC, memo } from "react";
 import { IconButton } from "@chakra-ui/button";
 import { Box, Flex, Heading, Link } from "@chakra-ui/layout";
 import { AddIcon } from "@chakra-ui/icons";
+import {
+  Drawer,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerBody,
+  DrawerOverlay,
+  useDisclosure,
+  Button,
+} from "@chakra-ui/react";
 
 export const Header: FC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Flex
-      as="nav"
-      bg="teal.500"
-      color="gray.50"
-      align="center"
-      justify="space-between"
-      padding={{ base: 3, md: 5 }}
-    >
-      <Flex align={"center"} as={"a"} mr={8} _hover={{ cursor: "pointer" }}>
-        <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
-          ユーザー管理アプリ
-        </Heading>
+    <>
+      <Flex
+        as="nav"
+        bg="teal.500"
+        color="gray.50"
+        align="center"
+        justify="space-between"
+        padding={{ base: 3, md: 5 }}
+      >
+        <Flex align={"center"} as={"a"} mr={8} _hover={{ cursor: "pointer" }}>
+          <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+            ユーザー管理アプリ
+          </Heading>
+        </Flex>
+
+        <Flex
+          align={"center"}
+          fontSize={"sm"}
+          flexGrow={2}
+          display={{ base: "none", md: "flex" }}
+        >
+          <Box pr={4}>
+            <Link>ユーザー一覧</Link>
+          </Box>
+          <Link>設定</Link>
+        </Flex>
+        <IconButton
+          aria-label={"menu-button"}
+          icon={<AddIcon />}
+          size={"sm"}
+          variant={"outline"}
+          display={{ base: "block", md: "none" }}
+          onClick={onOpen}
+        />
       </Flex>
 
-      <Flex
-        align={"center"}
-        fontSize={"sm"}
-        flexGrow={2}
-        display={{ base: "none", md: "flex" }}
-      >
-        <Box pr={4}>
-          <Link>ユーザー一覧</Link>
-        </Box>
-        <Link>設定</Link>
-      </Flex>
-      <IconButton
-        aria-label={"menu-button"}
-        icon={<AddIcon />}
-        size={"sm"}
-        variant={"outline"}
-        display={{ base: "block", md: "none" }}
-      ></IconButton>
-    </Flex>
+      <Drawer placement={"left"} size={"xs"} isOpen={isOpen} onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerBody p={0} bg={"gray.100"}>
+            <Button w={"100%"}>Top</Button>
+            <Button w={"100%"}>ユーザー一覧</Button>
+            <Button w={"100%"}>設定</Button>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 });
